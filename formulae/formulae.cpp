ee53cb1033957b6726a482cf28b41534a6ff12bc
PlaidSnowFrog <158831374+PlaidSnowFrog@users.noreply.github.com>
2025-01-23 14:02:28 +0000

    Added func to find centre of rect

--- formulae/formulae.cpp
+++ formulae/formulae.cpp
@@ -1,4 +1,5 @@
 #include "formulae.hpp"
+#include <SDL2/SDL.h>
 
 int findGradient(int A[2], int B[2]) {
     return ((A[1] - B[1]) / (A[0] - B[0]));
@@ -8,4 +9,9 @@ int findY(int originX, int aX, float gradient) {
     int finalX = {aX-originX};
      
     return gradient*finalX;
+}
+
+void findCentreOfRect(int *x, int *y, SDL_Rect rect) {
+    x = (rect.x + (rect.w/2);
+    y = (rect.y + (rect.h/2);
 }
